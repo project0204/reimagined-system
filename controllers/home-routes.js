c16534750fdae97d2ee92b2fe45448299c02ae18
Matthew Pryor <matthewapryor@gmail.com>
2022-05-26 20:02:59 -0400

    add pizza queries to home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,104 @@
+const { Pizza, Ingredient } = require('../models');
+
+const getPizza = (req, res) => {
+    Pizza.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'price',
+            'image_url',
+            'stock',
+            'ingredient_id',
+        ],
+        include: [
+            {
+                model: Ingredient,
+                attributes: ['pizza_id']
+            }
+        ]
+    })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+}
+
+const getAllPizzas = (req, res) => {
+    Pizza.findAll({
+        attributes: [
+            'id',
+            'title',
+            'price',
+            'image_url',
+            'stock',
+            'ingredient_id',
+        ],
+        include: [
+            {
+                model: Ingredient,
+                attributes: ['pizza_id']
+            }
+        ]
+    })
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+}
+
+const updatePizza = (req, res) => {
+    Pizza.update(
+        {
+            ingredient_id: req.body.ingredient_id
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbPizzaData => {
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+}
+
+const deletePizza = (req, res) => {
+    Pizza.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(500).json({ message: 'No pizza found with this id' })
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(dbPizzaData => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+}
+
+module.exports = {
+    getPizza,
+    getAllPizzas,
+    createPizza,
+    updatePizza,
+    deletePizza,
+}
