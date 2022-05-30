15a305651829e86ca5d58301a5c675fdf8532851
Villager <deiondrae@gmail.com>
2022-05-30 16:04:53 -0400

    updated pizza-seeds to match model

--- seeds/pizza-seeds.js
+++ seeds/pizza-seeds.js
@@ -6,6 +6,7 @@ const { Pizza } = require('../models')
 
 const pizzaData = [
     {
+        "id": 1,
         "title": "Pepperoni",
         "price": 19.99,
         "image_url": "https://www.jennycancook.com/wp-content/uploads/2013/02/PeppPizza_600.jpg",
@@ -13,6 +14,7 @@ const pizzaData = [
         "ingredients": []
     },
     {
+        "id": 2,
         "title": "Vegetarian",
         "price": 17.99,
         "image_url": "https://cookieandkate.com/images/2020/10/best-veggie-pizza-recipe-1-768x1152.jpg",
@@ -20,13 +22,15 @@ const pizzaData = [
         "ingredients": []
     },
     {
+        "id": 3,
         "title": "Hawaiian",
         "price": 19.99,
         "image_url": "https://www.jessicagavin.com/wp-content/uploads/2020/07/hawaiian-pizza-16-1024x1536.jpg",
-        "stock": 20,
+        "stock": 30,
         "ingredients": []
     },
     {
+        "id": 4,
         "title": "Buffalo Chicken",
         "price": 19.99,
         "image_url": "https://easychickenrecipes.com/wp-content/uploads/2019/11/buffalo-chicken-pizza-4.jpg",
@@ -34,28 +38,31 @@ const pizzaData = [
         "ingredients": []
     },
     {
+        "id": 5,
         "title": "Meat Lovers",
         "price": 17.99,
         "image_url": "https://www.queensleeappetit.com/wp-content/uploads/2019/02/Meat-Lovers-Pizza-5-1-480x480.jpg",
         "stock": 30,
         "ingredients": []
     },
-    // I think pizza pizza is trying tomake this "canadian" pizza a thing lol
     {
+        "id": 6,
         "title": "Canadian",
         "price": 19.99,
         "image_url": "https://storage.pizzapizza.ca/phx2/ppl_images/products/en/2x/canadian.png?cache_key=26",
-        "stock": 20,
+        "stock": 30,
         "ingredients": []
     },
     {
+        "id": 7,
         "title": "Pesto",
         "price": 19.99,
         "image_url": "https://www.jessicagavin.com/wp-content/uploads/2020/07/hawaiian-pizza-16-1024x1536.jpg",
-        "stock": 20,
+        "stock": 30,
         "ingredients": []
     },
     {
+        "id": 8,
         "title": "Mediterranean Veggie",
         "price": 19.99,
         "image_url": "https://www.foodiecrush.com/wp-content/uploads/2022/01/Mediterranean-Pizza-foodiecrush.com-63.jpg",
@@ -63,6 +70,7 @@ const pizzaData = [
         "ingredients": []
     },
     {
+        "id": 9,
         "title": "Mushroom Florentine",
         "price": 17.99,
         "image_url": "https://www.acouplecooks.com/wp-content/uploads/2019/06/Mushroom-Pizza-with-Herbs-011-800x1000.jpg",
