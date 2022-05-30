ae1b237fa884b2821435abb31195595313d9649b
Villager <deiondrae@gmail.com>
2022-05-30 15:19:59 -0400

    filled out ingredient and pizza seeds

--- seeds/pizza-seeds.js
+++ seeds/pizza-seeds.js
@@ -2,7 +2,7 @@ const { Pizza } = require('../models')
 
 // Pizza 1: Pepperoni
 // Pizza 2: Vegetarian
-// Pizza 3: Pineapple
+// Pizza 3: Hawaiian
 
 const pizzaData = [
     {
@@ -20,10 +20,53 @@ const pizzaData = [
         "ingredients": []
     },
     {
-        "title": "Pineapple",
+        "title": "Hawaiian",
         "price": 19.99,
         "image_url": "https://www.jessicagavin.com/wp-content/uploads/2020/07/hawaiian-pizza-16-1024x1536.jpg",
         "stock": 20,
+        "ingredients": []
+    },
+    {
+        "title": "Buffalo Chicken",
+        "price": 19.99,
+        "image_url": "https://easychickenrecipes.com/wp-content/uploads/2019/11/buffalo-chicken-pizza-4.jpg",
+        "stock": 30,
+        "ingredients": []
+    },
+    {
+        "title": "Meat Lovers",
+        "price": 17.99,
+        "image_url": "https://www.queensleeappetit.com/wp-content/uploads/2019/02/Meat-Lovers-Pizza-5-1-480x480.jpg",
+        "stock": 30,
+        "ingredients": []
+    },
+    // I think pizza pizza is trying tomake this "canadian" pizza a thing lol
+    {
+        "title": "Canadian",
+        "price": 19.99,
+        "image_url": "https://storage.pizzapizza.ca/phx2/ppl_images/products/en/2x/canadian.png?cache_key=26",
+        "stock": 20,
+        "ingredients": []
+    },
+    {
+        "title": "Pesto",
+        "price": 19.99,
+        "image_url": "https://www.jessicagavin.com/wp-content/uploads/2020/07/hawaiian-pizza-16-1024x1536.jpg",
+        "stock": 20,
+        "ingredients": []
+    },
+    {
+        "title": "Mediterranean Veggie",
+        "price": 19.99,
+        "image_url": "https://www.foodiecrush.com/wp-content/uploads/2022/01/Mediterranean-Pizza-foodiecrush.com-63.jpg",
+        "stock": 30,
+        "ingredients": []
+    },
+    {
+        "title": "Mushroom Florentine",
+        "price": 17.99,
+        "image_url": "https://www.acouplecooks.com/wp-content/uploads/2019/06/Mushroom-Pizza-with-Herbs-011-800x1000.jpg",
+        "stock": 30,
         "ingredients": []
     },
 ]
