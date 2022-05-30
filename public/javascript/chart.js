2b01854f398c89436d36e9e8e340bc1cfd4646d7
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-30 13:48:37 -0400

    added js for chart just need to verify how data will be returned

---
+++ public/javascript/chart.js
@@ -0,0 +1,50 @@
+const ctx = document.getElementById('myChart');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Stock',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+async function getData () { 
+    const response =await fetch()//from data base
+    const data = await response.text(); 
+
+    const table = data.split('/n').slice(1); //if data from db is not returned in array then change 
+    table.forEach(row => {
+        const columns = row.split(','); //change if data not returned in array 
+        const ingrediant = columnsp[0]; 
+        const stock = columns[1];
+        
+    })
+
+        
+
+}
