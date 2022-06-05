738cff66f14600c8d7d98c0f45b68c9e0a58bc32
ianaack <ianaack@gmail.com>
2022-06-05 09:43:50 -0400

    ran chart.js through Prettier cleaner
    removed a console.log for production

--- public/javascript/chart.js
+++ public/javascript/chart.js
@@ -1,85 +1,84 @@
-
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
 const allData = [];
 let stock1 = [];
 let name1 = [];
 
-
-
-
-async function getData () { 
-    const response =await fetch('/api/ingredients',{ 
-        method: 'GET',
-
-    });//from data base
+async function getData() {
+	const response = await fetch("/api/ingredients", {
+		method: "GET",
+	}); //from data base
 	if (response.ok) {
-        
-		response.json()
-        .then(function(data){
-            console.log(data);
-            name1 = [data[0].name, data[1].name,data[2].name,data[3].name,data[4].name,data[5].name]
-            stock1 = [data[0].stock, data[1].stock,data[2].stock,data[3].stock,data[4].stock,data[5].stock];
-            const ingredientChart = new Chart(ctx, {
+		response
+			.json()
+			.then(function (data) {
+				name1 = [
+					data[0].name,
+					data[1].name,
+					data[2].name,
+					data[3].name,
+					data[4].name,
+					data[5].name,
+				];
+				stock1 = [
+					data[0].stock,
+					data[1].stock,
+					data[2].stock,
+					data[3].stock,
+					data[4].stock,
+					data[5].stock,
+				];
+				const ingredientChart = new Chart(ctx, {
+					type: "bar", //other options are pie horizontal bar line doughnut radar and polararea
+					data: {
+						labels: name1,
+						datasets: [
+							{
+								label: "Stock",
+								data: stock1,
+								backgroundColor: [
+									"rgba(255, 99, 132, 0.2)",
+									"rgba(54, 162, 235, 0.2)",
+									"rgba(255, 206, 86, 0.2)",
+									"rgba(75, 192, 192, 0.2)",
+									"rgba(153, 102, 255, 0.2)",
+									"rgba(255, 159, 64, 0.2)",
+								],
+								borderColor: [
+									"rgba(255, 99, 132, 1)",
+									"rgba(54, 162, 235, 1)",
+									"rgba(255, 206, 86, 1)",
+									"rgba(75, 192, 192, 1)",
+									"rgba(153, 102, 255, 1)",
+									"rgba(255, 159, 64, 1)",
+								],
+								borderWidth: 1,
+							},
+						],
+					},
+					options: {
+						plugins: {
+							title: {
+								display: true,
+								text: "Stock Levels of Top Ingredients",
+							},
+						},
 
-                type: 'bar', //other options are pie horizontal bar line doughnut radar and polararea  
-                data: {
-                    labels: name1,
-                    datasets: [{
-                        label: 'Stock',
-                        data: stock1,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    plugins: {
-                        title: {
-                            display: true,
-                            text: 'Stock Levels of Top Ingredients'
-                        },
-                    },
-
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    },
-                    responsive: true,
-                    maintainAspectRatio: false
-
-                }
-            });
-        })
-        .catch(err => {console.log(err)})
-        
-    
-        
-	}else{
-		console.log("failed")
+						scales: {
+							y: {
+								beginAtZero: true,
+							},
+						},
+						responsive: true,
+						maintainAspectRatio: false,
+					},
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	} else {
+		console.log("failed");
 	}
-
-
-};
-
-// function filterData () {
-//     stock1 = allData[0].stock;
-//     // const filter = allData.filter(eachItem => eachItem.stock < 101);
-
-// }
+}
 
 getData();
